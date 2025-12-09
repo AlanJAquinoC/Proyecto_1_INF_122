4904716eb25afa3da56efd06d1fa1ce1bcbbb407
AlanJAquinoC <alanredimido.77@gmail.com>
2025-12-09 19:43:09 -0400

    Fix: Add basename to BrowserRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './Est_1.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./Est_1.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/Proyecto_1_INF_122">
     <App></App>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
